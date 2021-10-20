0f6bb48a84fa213c1a3593706348b303ffce5046
Bayekula-hb <hobedbayekula@gmail.com>
2021-10-20 11:22:35 +0100

    feat: add the new model

---
+++ src/models/historique_parcelle.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class historique_parcelle extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  historique_parcelle.init({
+    id_historique_parcelle: DataTypes.STRING,
+    parcelleId : DataTypes.INTEGER,
+    proprietaireId: DataTypes.INTEGER,
+    avenueId: DataTypes.INTEGER,
+    numero: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'historique_parcelle',
+  });
+  return historique_parcelle;
+};
