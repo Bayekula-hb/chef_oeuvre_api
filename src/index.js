d5697de6fb60ecf85db454f57c33e17317c65f44
Bayekula-hb <55344393+Bayekula-hb@users.noreply.github.com>
2021-12-25 21:40:22 +0100

    feat: authentication of a staff using their email or username

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const proprietaireRouter = require("./routers/proprietaires.router");
 const provinceRouter = require("./routers/provinces.router");
 const quarterRouter = require("./routers/quarters.route");
 const staffRouter = require("./routers/staffs.router");
+const { authRouter } = require("./routers/login.router");
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.get("/", async (req, res) => {
   res.send("Welcome to the app");
 });
+
+app.use("/api_lopango/auth", authRouter);
 app.use("/api_lopango/provinces", provinceRouter);
 app.use("/api_lopango/districts", districtRouter);
 app.use("/api_lopango/townships", townshipRouter);
--- src/routers/staffs.router.js
+++ src/routers/staffs.router.js
@@ -9,8 +9,10 @@ const staffRegisterMiddleware = require("../middlewares/staff/staff.register.val
 const getStaffMiddleware = require("../middlewares/staff/staff.get.middleware");
 
 const staffRouter = express.Router();
-module.exports = staffRouter;
+
 staffRouter.get("/", getStaffMiddleware, getOneStaff);
 staffRouter.get("/all", getAllStaff);
 staffRouter.post("/", staffRegisterMiddleware, addStaff);
 staffRouter.put("/", getStaffMiddleware, staffRegisterMiddleware, updateStaff);
+
+module.exports = staffRouter;
