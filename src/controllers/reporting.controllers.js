3602ca2dbcf1f39ff20c16dfee4f89803d99b0f9
Bayekula-hb <55344393+Bayekula-hb@users.noreply.github.com>
2022-07-15 14:03:14 +0100

    refactor: the get townships nlber

--- src/controllers/reporting.controllers.js
+++ src/controllers/reporting.controllers.js
@@ -17,12 +17,12 @@ const certificateCountByMounth = async (req, res) => {
       `SELECT COUNT(id_parcel) as number from parcels where MONTH(createdAt) = MONTH(now())`,
       { type: QueryTypes.SELECT }
     );
-    // const numberTownship = await sequelize.query(
-    //   `SELECT COUNT(id_parcel) as number from parcels inner join avenues on avenues.id = parcels.avenueId inner join quarters on quarters.id = avenues.quarterId inner join townshipId on townshipId.id = quarters.townshipId where MONTH(createdAt) = MONTH(now()) group by townshipId.id `,
-    //   { type: QueryTypes.SELECT }
-    // );
+    const numberTownship = await sequelize.query(
+      `SELECT COUNT(id) as number from townships`,
+      { type: QueryTypes.SELECT }
+    );
 
-    res.status(200).json({numberCertificate,numberParcel});
+    res.status(200).json({numberCertificate,numberParcel, numberTownship});
   } else {
     return res
       .status(400)
